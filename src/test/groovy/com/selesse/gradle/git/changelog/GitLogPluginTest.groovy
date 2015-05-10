7a0b72fac4dd8fedda2bad8cb520a1fe7a95a267
Alex Selesse <alex@selesse.com>
2015-05-09 22:18:20 -0400

    Move test file to proper directory

--- src/test/groovy/com/selesse/gradle/git/changelog/GitLogPluginTest.groovy
+++ src/test/groovy/com/selesse/gradle/git/changelog/GitLogPluginTest.groovy
@@ -1,11 +1,10 @@
 package com.selesse.gradle.git.changelog
-
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertTrue
 
 class GitLogPluginTest {
     @Test
