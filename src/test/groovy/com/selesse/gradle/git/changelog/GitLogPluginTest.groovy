bc2fc215378a4b26db33e57b7f489fbecaf1a775
Alex Selesse <alex@selesse.com>
2015-05-12 00:18:00 -0400

    Test outputDirectory setting

--- src/test/groovy/com/selesse/gradle/git/changelog/GitLogPluginTest.groovy
+++ src/test/groovy/com/selesse/gradle/git/changelog/GitLogPluginTest.groovy
@@ -4,14 +4,14 @@ import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
 
-import static org.junit.Assert.assertTrue
+import static org.assertj.core.api.Assertions.assertThat
 
 class GitLogPluginTest {
     @Test public void testGenerateChangelogTask_isProperInstance() {
         Project project = ProjectBuilder.builder().build()
         project.pluginManager.apply 'com.selesse.git.changelog'
 
-        assertTrue(project.tasks.generateChangelog instanceof GenerateChangelogTask)
+        assertThat(project.tasks.generateChangelog instanceof GenerateChangelogTask).isTrue()
     }
 
     @Test public void testAssemble_dependsOnGenerateChangelog() {
@@ -19,7 +19,8 @@ class GitLogPluginTest {
         project.pluginManager.apply 'base'
         project.pluginManager.apply 'com.selesse.git.changelog'
 
-        assertTrue(project.tasks.assemble.dependsOn.contains(project.tasks.generateChangelog))
+        assertThat(project.tasks.assemble.dependsOn).contains(project.tasks.generateChangelog)
+        assertThat(project.extensions.changelog.outputDirectory.absolutePath as String).endsWith('build')
     }
 
     @Test public void testProcessResources_dependsOnGenerateChangelog() {
@@ -27,12 +28,21 @@ class GitLogPluginTest {
         project.pluginManager.apply 'java'
         project.pluginManager.apply 'com.selesse.git.changelog'
 
-        assertTrue(project.tasks.processResources.dependsOn.contains(project.tasks.generateChangelog))
+        assertThat(project.tasks.processResources.dependsOn).contains(project.tasks.generateChangelog)
+        assertThat(project.extensions.changelog.outputDirectory.absolutePath as String).endsWith('build/resources/main')
 
         project = ProjectBuilder.builder().build()
         project.pluginManager.apply 'groovy'
         project.pluginManager.apply 'com.selesse.git.changelog'
 
-        assertTrue(project.tasks.processResources.dependsOn.contains(project.tasks.generateChangelog))
+        assertThat(project.tasks.processResources.dependsOn).contains(project.tasks.generateChangelog)
+        assertThat(project.extensions.changelog.outputDirectory.absolutePath as String).endsWith('build/resources/main')
+
+        project = ProjectBuilder.builder().build()
+        project.pluginManager.apply 'war'
+        project.pluginManager.apply 'com.selesse.git.changelog'
+
+        assertThat(project.tasks.processResources.dependsOn).contains(project.tasks.generateChangelog)
+        assertThat(project.extensions.changelog.outputDirectory.absolutePath as String).endsWith('build/resources/main')
     }
 }
