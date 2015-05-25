f76eb946e82a1f85bbb6dbc3c22072d067509672
Alex Selesse <alex@selesse.com>
2015-05-25 19:44:24 -0400

    [BUG] Fix plugin not working if Java/Base plugins are applied **after** this plugin

--- src/main/groovy/com/selesse/gradle/git/changelog/GitLogPlugin.groovy
+++ src/main/groovy/com/selesse/gradle/git/changelog/GitLogPlugin.groovy
@@ -1,30 +1,26 @@
 package com.selesse.gradle.git.changelog
-
 import com.selesse.gradle.git.changelog.tasks.GenerateChangelogTask
-import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 import org.gradle.api.plugins.BasePlugin
+import org.gradle.api.plugins.JavaPlugin
 
 class GitLogPlugin implements Plugin<Project> {
     Logger logger = Logging.getLogger(GitLogPlugin)
     private GitChangelogExtension extension
-    private File defaultOutputDirectory
 
     @Override
     void apply(Project project) {
-        defaultOutputDirectory = project.buildDir
-
         applyPluginDependency(project)
 
         extension = project.extensions.create("changelog", GitChangelogExtension)
 
         extension.with {
             title = project.name
-            outputDirectory = defaultOutputDirectory
+            outputDirectory = project.buildDir
             fileName = 'CHANGELOG.md'
             since = 'beginning'
             commitFormat = '%ad%x09%s (%an)'
@@ -38,22 +34,29 @@ class GitLogPlugin implements Plugin<Project> {
     def applyPluginDependency(Project project) {
         GenerateChangelogTask task = project.tasks.create("generateChangelog", GenerateChangelogTask)
 
-        if (project.tasks.findByName('processResources') != null) {
-            Task processResources = project.tasks.processResources
-            if (processResources != null) {
-                logger.debug("Making processResources task depend on ${task.name}")
-                processResources.dependsOn(task)
+        project.afterEvaluate {
+            project.plugins.withType(JavaPlugin) {
+                logger.info("Configuring Java plugin")
+                Task processResources = project.tasks.processResources
+                if (processResources != null) {
+                    logger.debug("Making assembleTask depend on ${task.name}")
+                    processResources.dependsOn(task)
 
-                defaultOutputDirectory = processResources.destinationDir
+                    logger.debug("Setting destination directory to {}", processResources.destinationDir)
+                    project.extensions.changelog.outputDirectory = processResources.destinationDir
+                }
             }
-        } else if (project.plugins.hasPlugin(BasePlugin)) {
-            DefaultTask assembleTask = project.tasks.getByName("assemble") as DefaultTask
-            if (assembleTask != null) {
-                logger.debug("Making assemble task depend on ${task.name}")
-                assembleTask.dependsOn(task)
+
+            if (project.plugins.findPlugin(JavaPlugin) == null) {
+                project.plugins.withType(BasePlugin) {
+                    logger.debug("Configuring Base Plugin")
+                    Task assembleTask = project.tasks.getByName("assemble") as Task
+                    if (assembleTask != null) {
+                        logger.debug("Making assembleTask depend on ${task.name}")
+                        assembleTask.dependsOn(task)
+                    }
+                }
             }
-        } else {
-            logger.info('Base plugin could not be found, tasks will not be injected')
         }
     }
 }
